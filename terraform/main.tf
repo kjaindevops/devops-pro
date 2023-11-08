085efe9af89ae00c2a4c90393912d48072d7d71d
kjaindevops <k.jain.devops@gmail.com>
2023-11-08 16:35:22 +0530

    Modified the code for creating the vpc and the required resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,74 @@ data "aws_ami" "demo-ami" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*.1-kernel-6.1-x86_64"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+}
+
+resource "aws_vpc" "client-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "client-vpc"
+  }
+}
+
+resource "aws_subnet" "client-pubsub-01" {
+  vpc_id                  = aws_vpc.client-vpc.id
+  cidr_block              = "10.1.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "client-pubsub-01"
+  }
+}
+
+resource "aws_subnet" "client-pubsub-02" {
+  vpc_id                  = aws_vpc.client-vpc.id
+  cidr_block              = "10.1.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "client-pubsub-02"
+  }
+}
+
+resource "aws_internet_gateway" "client-igw" {
+  vpc_id = aws_vpc.client-vpc.id
+  tags = {
+    Name = "client-igw"
+  }
+}
+
+resource "aws_route_table" "client-pub-rt" {
+  vpc_id = aws_vpc.client-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.client-igw.id
+  }
+
+  tags = {
+    Name = "client-pub-rt"
+  }
+}
+
+resource "aws_route_table_association" "client-pub-rta1" {
+  subnet_id      = aws_subnet.client-pubsub-01.id
+  route_table_id = aws_route_table.client-pub-rt.id
+
+}
+
+resource "aws_route_table_association" "client-pub-rta2" {
+  subnet_id      = aws_subnet.client-pubsub-02.id
+  route_table_id = aws_route_table.client-pub-rt.id
 }
 
 resource "aws_security_group" "ssh-allow" {
   name        = "ssh-allow"
   description = "Allowing SSH to the server."
+  vpc_id      = aws_vpc.client-vpc.id
 
   ingress {
     description = "Allowing SSH access from Macbook"
@@ -34,8 +95,8 @@ resource "aws_security_group" "ssh-allow" {
 
 
 resource "aws_instance" "demo-server" {
-  ami             = data.aws_ami.demo-ami.id
-  key_name        = "DevOps-Mac-kp"
-  security_groups = ["ssh-allow"]
-  instance_type   = "t2.micro"
+  ami                    = data.aws_ami.demo-ami.id
+  key_name               = "DevOps-Mac-kp"
+  vpc_security_group_ids = [aws_security_group.ssh-allow.id]
+  instance_type          = "t2.micro"
 }
